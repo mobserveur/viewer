593e7199e2dece84584882ed90b5dcc0ffb4d52b
Erik Kundiman <erik@megapahit.org>
2023-07-11 19:29:32 +0800

    Obtain OpenJPEG flags when using system libs
    The module name to check with pkgconf is different, hence the special
    treatment.

--- indra/cmake/OpenJPEG.cmake
+++ indra/cmake/OpenJPEG.cmake
@@ -4,6 +4,15 @@ include(Prebuilt)
 include_guard()
 add_library( ll::openjpeg INTERFACE IMPORTED )
 
+if (NOT (USE_AUTOBUILD_3P OR USE_CONAN))
+  include(FindPkgConfig)
+  pkg_check_modules(Openjpeg REQUIRED libopenjp2)
+  target_include_directories(ll::openjpeg SYSTEM INTERFACE ${Openjpeg_INCLUDE_DIRS})
+  target_link_directories(ll::openjpeg INTERFACE ${Openjpeg_LIBRARY_DIRS})
+  target_link_libraries(ll::openjpeg INTERFACE ${Openjpeg_LIBRARIES})
+  return ()
+endif ()
+
 use_system_binary(openjpeg)
 use_prebuilt_binary(openjpeg)
 
