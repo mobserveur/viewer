9a3c16da3bd845964de4bc9dfe297bb3901ad84f
Erik Kundiman <erik@megapahit.org>
2023-07-11 13:53:54 +0800

    Only make sure there's xxHash when using sys libs

--- indra/cmake/xxHash.cmake
+++ indra/cmake/xxHash.cmake
@@ -5,4 +5,11 @@ endif (XXHASH_CMAKE_INCLUDED)
 set (XXHASH_CMAKE_INCLUDED TRUE)
 
 include(Prebuilt)
+
+if (NOT (USE_AUTOBUILD_3P OR USE_CONAN))
+  include(FindPkgConfig)
+  pkg_check_modules(Xxhash REQUIRED libxxhash)
+  return ()
+endif ()
+
 use_prebuilt_binary(xxhash)
