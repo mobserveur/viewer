c718c518f86e3bd78bd71bbd07fbc626373bbe96
Erik Kundiman <erik@megapahit.org>
2023-07-11 15:45:07 +0800

    Not use use_system_binary for system GLEXT
    for the same reason as GLH, but since the headers are expected to be
    installed in the same directory as GLH (and GLEXT.cmake includes
    GLH.cmake), we can skip any additional directory to look the headers for.

--- indra/cmake/GLEXT.cmake
+++ indra/cmake/GLEXT.cmake
@@ -3,6 +3,11 @@ include(Prebuilt)
 include(GLH)
 
 add_library( ll::glext INTERFACE IMPORTED )
+
+if (NOT (USE_AUTOBUILD_3P OR USE_CONAN))
+  return ()
+endif ()
+
 if (WINDOWS OR LINUX)
   use_system_binary(glext)
   use_prebuilt_binary(glext)
