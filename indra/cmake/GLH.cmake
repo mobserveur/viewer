b4d1301f272e707548f30f52671db76576273eaa
Erik Kundiman <erik@megapahit.org>
2023-07-11 14:58:08 +0800

    Not use use_system_binary for system GLH
    No package manager that I know of provide such package. So this one is
    expected to be installed in /usr/local/include.

--- indra/cmake/GLH.cmake
+++ indra/cmake/GLH.cmake
@@ -3,5 +3,10 @@ include(Prebuilt)
 
 add_library( ll::glh_linear INTERFACE IMPORTED )
 
+if (NOT (USE_AUTOBUILD_3P OR USE_CONAN))
+  target_include_directories( ll::glh_linear SYSTEM INTERFACE /usr/local/include )
+  return ()
+endif ()
+
 use_system_binary( glh_linear )
 use_prebuilt_binary(glh_linear)
