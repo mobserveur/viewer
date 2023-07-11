79b91a4ed2d826308e5dd2c9493efb449b380ff7
Erik Kundiman <erik@megapahit.org>
2023-07-11 14:28:24 +0800

    Not use use_system_binary for system xmlrpc-epi
    The xmlrpc-epi package has no .pc or .cmake files. On some platforms,
    the header and the library directories don't have special paths. On
    GNU/Linux, at least on Debian, the headers are encapsulated in the
    packages's own directory. On macOS, both MacPorts & Homebrew don't have
    the package. On the other hand, Fink, that has the package, still
    doesn't support recent versions of macOS as of this writing. So it's
    very likely that on macOS, xmlrpc-epi is installed in /usr/local.

--- indra/cmake/XmlRpcEpi.cmake
+++ indra/cmake/XmlRpcEpi.cmake
@@ -4,8 +4,18 @@ include(Prebuilt)
 include_guard()
 add_library( ll::xmlrpc-epi INTERFACE IMPORTED )
 
+if (USE_AUTOBUILD_3P OR USE_CONAN)
 use_system_binary( xmlrpc-epi )
 
 use_prebuilt_binary(xmlrpc-epi)
+endif ()
+
 target_link_libraries(ll::xmlrpc-epi INTERFACE xmlrpc-epi )
+
+if (USE_AUTOBUILD_3P OR USE_CONAN)
 target_include_directories( ll::xmlrpc-epi SYSTEM INTERFACE ${LIBS_PREBUILT_DIR}/include)
+elseif (LINUX)
+target_include_directories( ll::xmlrpc-epi SYSTEM INTERFACE /usr/include/xmlrpc-epi)
+elseif (DARWIN)
+target_link_directories( ll::xmlrpc-epi SYSTEM INTERFACE /usr/local/lib)
+endif ()
