2fc3845e1b23a1efd5666241a2d1cc78340d4b61
Erik Kundiman <erik@megapahit.org>
2023-07-11 18:54:50 +0800

    Obtain FontConfig flags when using system libs
    and at the same time escape use_prebuilt_binary commands in the file.

--- indra/cmake/ViewerMiscLibs.cmake
+++ indra/cmake/ViewerMiscLibs.cmake
@@ -1,6 +1,12 @@
 # -*- cmake -*-
 include(Prebuilt)
 
+if (NOT (USE_AUTOBUILD_3P OR USE_CONAN))
+  add_library( ll::fontconfig INTERFACE IMPORTED )
+  use_system_binary(fontconfig)
+  return ()
+endif ()
+
 if (LINUX)
   #use_prebuilt_binary(libuuid)
   add_library( ll::fontconfig INTERFACE IMPORTED )
