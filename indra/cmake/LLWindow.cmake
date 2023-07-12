fe29edcff99d5e69bb4cf401870f7503089cf69c
Erik Kundiman <erik@megapahit.org>
2023-07-12 15:42:07 +0800

    Use system SDL on FreeBSD too
    X11 is still required too, that's why it's still added for linking.
    On Darwin, SDL is disabled for now.

--- indra/cmake/LLWindow.cmake
+++ indra/cmake/LLWindow.cmake
@@ -7,6 +7,16 @@ include(Prebuilt)
 include_guard()
 add_library( ll::SDL INTERFACE IMPORTED )
 
+if (NOT (USE_AUTOBUILD_3P OR USE_CONAN) AND NOT DARWIN)
+  include(FindPkgConfig)
+  pkg_check_modules(Sdl REQUIRED sdl)
+  target_compile_definitions( ll::SDL INTERFACE LL_SDL=1)
+  target_include_directories(ll::SDL SYSTEM INTERFACE ${Sdl_INCLUDE_DIRS})
+  target_link_directories(ll::SDL INTERFACE ${Sdl_LIBRARY_DIRS})
+  target_link_libraries(ll::SDL INTERFACE ${Sdl_LIBRARIES} X11)
+  return ()
+endif ()
+
 
 if (LINUX)
   #Must come first as use_system_binary can exit this file early
