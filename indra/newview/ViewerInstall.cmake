00dc394684356a21d02a798f9ad098587381d142
Erik Kundiman <erik@megapahit.org>
2023-08-26 16:39:32 +0800

    Certificate authority bundle gets installed too
    When I tried using, for example, FBSD system's ca-root-nss.crt, at
    runtime, the viewer would fail at downloading textures, avatar names,
    and so on. So for now we're still relying on LLCA, it's just get
    installed automatically without having to track the file in the viewer
    project.

--- indra/newview/ViewerInstall.cmake
+++ indra/newview/ViewerInstall.cmake
@@ -21,6 +21,7 @@ endif (IS_ARTWORK_PRESENT)
 
 install(FILES featuretable_linux.txt
         #featuretable_solaris.txt
+        ${AUTOBUILD_INSTALL_DIR}/ca-bundle.crt
         DESTINATION share/${VIEWER_BINARY_NAME}
         )
 
