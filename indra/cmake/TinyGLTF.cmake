2a1565fa402578d2170b0e86db99c9cb2fdab1cb
Erik Kundiman <erik@megapahit.org>
2023-09-10 20:30:24 +0800

    TinyGLTF & Vulkan glTF not included on systemlibs
    Vulkan glTF doesn't seem to be used yet.
    TINYGLTF_INCLUDE_DIR doesn't seem to be used yet either.
    Tiny GLTF can be headers only, easy to install to system.

--- indra/cmake/TinyGLTF.cmake
+++ indra/cmake/TinyGLTF.cmake
@@ -1,7 +1,11 @@
 # -*- cmake -*-
 include(Prebuilt)
 
+if (NOT USESYSTEMLIBS)
+
 use_prebuilt_binary(tinygltf)
 
 set(TINYGLTF_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include/tinygltf) 
+
+endif (NOT USESYSTEMLIBS)
 
--- indra/cmake/VulkanGltf.cmake
+++ indra/cmake/VulkanGltf.cmake
@@ -1,5 +1,7 @@
 # -*- cmake -*-
 include(Prebuilt)
 
+if (NOT USESYSTEMLIBS)
 use_prebuilt_binary(vulkan_gltf)
+endif ()
 
