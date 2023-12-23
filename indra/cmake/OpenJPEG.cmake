9ad01c1823259ba5a5710289b65d629732199eaf
Erik Kundiman <erik@megapahit.org>
2023-12-23 19:42:32 +0800

    Rely on prebuilt OpenJPEG headers
    so we follow its encapsulating directory naming.
    Looks like we're still going to be using LL's 3p-openjpeg for a while
    more.

--- indra/cmake/OpenJPEG.cmake
+++ indra/cmake/OpenJPEG.cmake
@@ -17,4 +17,4 @@ add_library( ll::openjpeg INTERFACE IMPORTED )
 #use_prebuilt_binary(openjpeg)
 
 target_link_libraries(ll::openjpeg INTERFACE openjp2 )
-target_include_directories( ll::openjpeg SYSTEM INTERFACE ${LIBS_PREBUILT_DIR}/include/openjpeg)
+target_include_directories( ll::openjpeg SYSTEM INTERFACE ${LIBS_PREBUILT_DIR}/include)
