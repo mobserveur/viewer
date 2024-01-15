4042e8c86a2e244d7fc531ad4645af17047d9d61
Erik Kundiman <erik@megapahit.org>
2024-01-15 12:27:08 +0800

    Re-enable GL colour bits check but only on Linux
    It seems the queries don't work on BSDs, but works okay on Linuxes.

--- indra/llwindow/llwindowsdl.cpp
+++ indra/llwindow/llwindowsdl.cpp
@@ -723,7 +723,7 @@ BOOL LLWindowSDL::createContext(int x, int y, int width, int height, int bits, B
 	// relaxed about if we have to.
 	if (colorBits < 32)
 	{
-/*
+#if LL_LINUX
 		close();
 		setupFailure(
 			"Second Life requires True Color (32-bit) to run in a window.\n"
@@ -734,7 +734,7 @@ BOOL LLWindowSDL::createContext(int x, int y, int width, int height, int bits, B
 			"Error",
 			OSMB_OK);
 		return FALSE;
-*/
+#endif // LL_LINUX
 	}
 
 #if 0  // *FIX: we're going to brave it for now...
