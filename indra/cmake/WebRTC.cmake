b6c1ba50d4d4665249fbc210c27db8c2f4942dd4
Erik Kundiman <erik@megapahit.org>
2024-06-27 20:55:30 +0800

    Prevent automatic download of WebRTC
    for when autobuild's not installed.

--- indra/cmake/WebRTC.cmake
+++ indra/cmake/WebRTC.cmake
@@ -6,7 +6,9 @@ include_guard()
 
 add_library( ll::webrtc INTERFACE IMPORTED )
 target_include_directories( ll::webrtc SYSTEM INTERFACE "${LIBS_PREBUILT_DIR}/include/webrtc" "${LIBS_PREBUILT_DIR}/include/webrtc/third_party/abseil-cpp")
+if (NOT USESYSTEMLIBS)
 use_prebuilt_binary(webrtc)
+endif ()
 
 if (WINDOWS)
     target_link_libraries( ll::webrtc INTERFACE webrtc.lib )
