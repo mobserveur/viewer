630a1197d219e57098236913bbd464d6b03f2a6d
Erik Kundiman <erik@megapahit.org>
2024-07-28 08:26:38 +0800

    Make system GLM found
    I had to use find_package here directly because somehow our
    "different" use_system_binary wouldn't set glm_FOUND to true, even
    though CMake found the package.

--- indra/cmake/GLM.cmake
+++ indra/cmake/GLM.cmake
@@ -3,5 +3,9 @@ include(Prebuilt)
 
 add_library( ll::glm INTERFACE IMPORTED )
 
+if (USESYSTEMLIBS)
+  find_package( glm REQUIRED )
+else ()
 use_system_binary( glm )
 use_prebuilt_binary(glm)
+endif ()
