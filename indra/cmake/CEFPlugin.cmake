07004adb7e52b7ead801251b13e8589ad3971f6c
Erik Kundiman <erik@megapahit.org>
2024-08-10 07:30:46 +0800

    Fix 7c716d6d516f99628655352a0e679455263924e2 logics

--- indra/cmake/CEFPlugin.cmake
+++ indra/cmake/CEFPlugin.cmake
@@ -5,21 +5,23 @@ include(Prebuilt)
 include_guard()
 add_library( ll::cef INTERFACE IMPORTED )
 
-if (CMAKE_OSX_ARCHITECTURES MATCHES arm64 AND (${PREBUILD_TRACKING_DIR}/sentinel_installed IS_NEWER_THAN ${PREBUILD_TRACKING_DIR}/dullahan_installed OR NOT ${dullahan_installed} EQUAL 0))
-    execute_process(COMMAND curl
-        -O
-        https://megapahit.net/downloads/dullahan-1.14.0.202312131437_118.7.1_g99817d2_chromium-118.0.5993.119-darwin64-242070244.tar.bz2
-        WORKING_DIRECTORY $ENV{HOME}/Downloads
-        )
-    execute_process(COMMAND tar
-        xf
-        $ENV{HOME}/Downloads/dullahan-1.14.0.202312131437_118.7.1_g99817d2_chromium-118.0.5993.119-darwin64-242070244.tar.bz2
-        WORKING_DIRECTORY ${AUTOBUILD_INSTALL_DIR}
-        RESULT_VARIABLE dullahan_installed
-        )
+if (CMAKE_OSX_ARCHITECTURES MATCHES arm64)
+    if (${PREBUILD_TRACKING_DIR}/sentinel_installed IS_NEWER_THAN ${PREBUILD_TRACKING_DIR}/dullahan_installed OR NOT ${dullahan_installed} EQUAL 0)
+        execute_process(COMMAND curl
+            -O
+            https://megapahit.net/downloads/dullahan-1.14.0.202312131437_118.7.1_g99817d2_chromium-118.0.5993.119-darwin64-242070244.tar.bz2
+            WORKING_DIRECTORY $ENV{HOME}/Downloads
+            )
+        execute_process(COMMAND tar
+            xf
+            $ENV{HOME}/Downloads/dullahan-1.14.0.202312131437_118.7.1_g99817d2_chromium-118.0.5993.119-darwin64-242070244.tar.bz2
+            WORKING_DIRECTORY ${AUTOBUILD_INSTALL_DIR}
+            RESULT_VARIABLE dullahan_installed
+            )
+    endif (${PREBUILD_TRACKING_DIR}/sentinel_installed IS_NEWER_THAN ${PREBUILD_TRACKING_DIR}/dullahan_installed OR NOT ${dullahan_installed} EQUAL 0)
 else ()
 use_prebuilt_binary(dullahan)
-endif ()
+endif (CMAKE_OSX_ARCHITECTURES MATCHES arm64)
 
 target_include_directories( ll::cef SYSTEM INTERFACE  ${LIBS_PREBUILT_DIR}/include/cef)
 
