94c9e0a17bc15c6f0f691484b0b616726b9b10fc
Erik Kundiman <erik@megapahit.org>
2025-02-15 14:58:13 +0800

    Boost 1.87 ready while still backwards compatible
    Arch system Boost has been upgraded to 1.87, and some things are
    deprecated.
    
    https://stackoverflow.com/questions/78821440/iterating-over-buffers-in-an-asiostreambuf
    
    https://github.com/badaix/snapcast/issues/1305

--- indra/llcommon/llprocess.cpp
+++ indra/llcommon/llprocess.cpp
@@ -176,13 +176,13 @@ public:
             // In general, our streambuf might contain a number of different
             // physical buffers; iterate over those.
             bool keepwriting = true;
-            for (const_buffer_sequence::const_iterator bufi(bufs.begin()), bufend(bufs.end());
+            for (auto bufi = buffer_sequence_begin(bufs), bufend = buffer_sequence_end(bufs);
                  bufi != bufend && keepwriting; ++bufi)
             {
                 // http://www.boost.org/doc/libs/1_49_0_beta1/doc/html/boost_asio/reference/buffer.html#boost_asio.reference.buffer.accessing_buffer_contents
                 // Although apr_file_write() accepts const void*, we
                 // manipulate const char* so we can increment the pointer.
-                const char* remainptr = boost::asio::buffer_cast<const char*>(*bufi);
+                const char* remainptr = static_cast<const char*>((*bufi).data());
                 std::size_t remainlen = boost::asio::buffer_size(*bufi);
                 while (remainlen)
                 {
@@ -377,14 +377,14 @@ public:
             // In general, the mutable_buffer_sequence returned by prepare() might
             // contain a number of different physical buffers; iterate over those.
             std::size_t tocommit(0);
-            for (mutable_buffer_sequence::const_iterator bufi(bufs.begin()), bufend(bufs.end());
+            for (auto bufi = buffer_sequence_begin(bufs), bufend = buffer_sequence_end(bufs);
                  bufi != bufend; ++bufi)
             {
                 // http://www.boost.org/doc/libs/1_49_0_beta1/doc/html/boost_asio/reference/buffer.html#boost_asio.reference.buffer.accessing_buffer_contents
                 std::size_t toread(boost::asio::buffer_size(*bufi));
                 apr_size_t gotten(toread);
                 apr_status_t err = apr_file_read(mPipe,
-                                                 boost::asio::buffer_cast<void*>(*bufi),
+                                                 static_cast<void*>((*bufi).data()),
                                                  &gotten);
                 // EAGAIN is exactly what we want from a nonblocking pipe.
                 // Rather than waiting for data, it should return immediately.
